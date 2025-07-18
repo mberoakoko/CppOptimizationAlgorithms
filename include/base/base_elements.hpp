515bbab6585eeb08961fa87958ee1652c9657816
mbero_akoko <mberoakoko@gmail.com>
2025-07-18 18:28:47 +0200

    Added some changes

--- include/base/base_elements.hpp
+++ include/base/base_elements.hpp
@@ -39,15 +39,18 @@ namespace Optim::diff {
 
     namespace details_ {
         constexpr auto visitor_ = overloads {
-            [](base::VectorType_d&  val) -> base::VectorType_d{ return val;},
-            [](base::VectorType_f&  val) -> base::VectorType_f{ return val;},
-            [](base::VectorType_i&  val)-> base::VectorType_i{ return val;},
+            [](const base::VectorType_d&  val) -> base::VectorType_d{ return val;},
+            [](base::VectorType_d&  val) -> base::VectorType_d&{ return val;},
+            [](const base::VectorType_f&  val) -> base::VectorType_f{ return val;},
+            [](base::VectorType_f&  val) -> base::VectorType_f&{ return val;},
+            [](const base::VectorType_i&  val)-> base::VectorType_i{ return val;},
+            [](base::VectorType_i&  val)-> base::VectorType_i&{ return val;},
         };
     }
 
 
     class Diff {
-        static void forward_diff_impl(base::FuncType func, base::InputType x, base::InputType h) -> base::OutputType {
+        static void forward_diff_impl(const base::FuncType& func, const base::InputType& x, const base::InputType& h) -> base::OutputType {
             if (std::holds_alternative<float>(x) and std::holds_alternative<float>(h)) {
                 const auto x_ = std::get<double>(x) ;
                 const auto h_ = std::get<double>(h);
@@ -58,31 +61,45 @@ namespace Optim::diff {
             return func(h_);
         };
 
-        static void backward_diff_impl(base::FuncType func, base::InputType x, base::InputType h) -> base::OutputType {
-
+        static void backward_diff_impl(const base::FuncType& func, const base::InputType& x, const base::InputType& h) -> base::OutputType {
+            if (std::holds_alternative<float>(x) and std::holds_alternative<float>(h)) {
+                const auto x_ = std::get<double>(x) ;
+                const auto h_ = std::get<double>(h);
+                return (1/h_) * (func(x_ + h_) - func(x_));
+            }
+            const auto x_ = std::visit(details_::visitor_, std::get<base::VectorVar>(x));
+            const auto h_ = std::visit(details_::visitor_, std::get<base::VectorVar>(h));
+            return func(h_);
         }
 
-        static void central_diff_impl(base::FuncType func, base::InputType x, base::InputType h) -> base::OutputType {
-
+        static void central_diff_impl(const base::FuncType& func, const base::InputType& x, const base::InputType& h) -> base::OutputType {
+            if (std::holds_alternative<float>(x) and std::holds_alternative<float>(h)) {
+                const auto x_ = std::get<double>(x) ;
+                const auto h_ = std::get<double>(h);
+                return (1/h_) * (func(x_ + h_) - func(x_));
+            }
+            const auto x_ = std::visit(details_::visitor_, std::get<base::VectorVar>(x));
+            const auto h_ = std::visit(details_::visitor_, std::get<base::VectorVar>(h));
+            return func(h_);
         }
 
-        static void dispatcher(const ForwardDiff tag, const base::FuncType& func, base::InputType x, base::InputType h) -> base::OutputType {
+        static void dispatcher(const ForwardDiff tag, const base::FuncType& func, const base::InputType& x, const base::InputType& h) -> base::OutputType {
             static_cast<void>(tag);
             return forward_diff_impl(func, x, h);
         };
 
-        static void dispatcher(BackWardDiff tag, base::FuncType func, base::InputType x, base::InputType h) -> base::OutputType {
+        static void dispatcher(const BackWardDiff& tag, const base::FuncType& func, const base::InputType& x, const base::InputType& h) -> base::OutputType {
             static_cast<void>(tag);
             return backward_diff_impl(func, x, h);
         }
 
-        static void dispatcher(CentralDiff tag, base::FuncType func, base::InputType x, base::InputType h) {
+        static void dispatcher(const CentralDiff& tag, const base::FuncType& func, const base::InputType& x, const base::InputType& h) -> base::OutputType {
             static_cast<void>(tag);
             return central_diff_impl(func, x, h);
         }
     public:
         template<ValidDiffTag Tag>
-        static auto eval(base::FuncType func, base::InputType x, base::InputType h) {
+        static auto eval(const base::FuncType& func, const base::InputType& x, const base::InputType& h) {
             dispatcher(Tag{}, func, x, h);
         };
     };
